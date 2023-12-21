a39d428a98fac4acccb997a4ec09ab8b65add40c
Pasindu Rathnathilake <ikpirathnathilake@students.nsbm.ac.lk>
2023-12-21 21:55:52 +0530

    create get user and add user functions

--- backend/controller.js
+++ backend/controller.js
@@ -1,21 +1,29 @@
-const users = [
-  {
-    id: 1,
-    name: "Pasindu",
-  },
-  {
-    id: 2,
-    name: "Induwara",
-  },
-];
+const { response } = require("express");
+const User = require("./model");
 
-const getUsers = (cb) => {
-  cb(users);
+const getUsers = (req, res, next) => {
+  User.find()
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({ error });
+    });
 };
 
-const getUserById = (id, cb) => {
-  const user = users.find((user) => user.id == id);
-  cb(user);
+const addUser = (req, res, next) => {
+  const user = new User({
+    id: req.body.id,
+    name: req.body.name,
+  });
+  user
+    .save()
+    .then((response) => {
+      res.json({ response });
+    })
+    .catch((error) => {
+      res.json({ error });
+    });
 };
 
 exports.getUsers = getUsers;
